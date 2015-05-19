2760db3a6d8ac274feb1a60b61ed82470ee1c56e
David Bustillo <hbustillo24@hotmail.com>
2015-05-19 03:59:07 -0600

    :)

--- ExamenDBustillo/ExamenDBustillo/App_Start/RouteConfig.cs
+++ ExamenDBustillo/ExamenDBustillo/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ExamenDBustillo
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Complejo", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
